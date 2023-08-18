dfe0e4c18038764d081522026a325440410930be
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-08-18 19:29:09 +0300

    in views/py modified all views according to changed serializers, added filtering in AstronomyShowViewSet, ShowSessionViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from django.db.models import Count, F
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 
 from planetarium.models import (
@@ -17,11 +22,15 @@ from planetarium.serializers import (
     ShowSessionListSerializer,
     ShowSessionDetailSerializer,
     ReservationSerializer,
-    TicketSerializer, PlanetariumDomeListSerializer,
+    TicketSerializer,
+    PlanetariumDomeListSerializer,
+    ReservationListSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer,
+    AstronomyShowImageSerializer,
 )
 
 
-# Create your views here.
 class ShowThemeViewSet(viewsets.ModelViewSet):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializers
@@ -52,14 +61,64 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
     serializer_class = AstronomyShowSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
+    def get_queryset(self):
+        """Retrieve the astronomy show with filters"""
+        title = self.request.query_params.get("title")
+        theme = self.request.query_params.get("theme")
+        queryset = self.queryset
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if theme:
+            queryset = queryset.filter(theme__icontains=theme)
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+        if self.action == "upload_image":
+            return AstronomyShowImageSerializer
+        return AstronomyShowSerializer
+
 
 class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.select_related(
         "astronomy_show", "planetarium_dome"
-    )
+    ).annotate(tickets_available=(F("planetarium_dome__rows") * F("planetarium_dome__seats_in_row") - Count("ticket")))
     serializer_class = ShowSessionSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        astronomy_show_id_str = self.request.query_params.get("astronomy_show")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if astronomy_show_id_str:
+            queryset = queryset.filter(movie_id=int(astronomy_show_id_str))
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="astronomy_show",
+                type=int,
+                description="Filter by astronomy show id (ex: ?movie=2)",
+            ),
+            OpenApiParameter(
+                name="date",
+                type=OpenApiTypes.DATE,
+                description="Filter by date show session "
+                            "(ex: ?date=2024-10-15)"
+            ),
+        ]
+    )
     def get_serializer_class(self):
         if self.action == "list":
             return ShowSessionListSerializer
@@ -70,9 +129,20 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.select_related("user")
+    queryset = Reservation.objects.prefetch_related(
+        "ticket__show_session__planetarium_dome",
+        "ticket__show_session__astronomy_show",
+    )
     serializer_class = ReservationSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+        return ReservationSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
