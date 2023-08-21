eeee6b927a1e799754fad999245c74f410ae248d
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-08-21 20:05:29 +0300

    create  MovieImageUploadTests part of Astronomy show  test case. But one test failed. Need to fix.

--- planetarium/tests/test_astronomy_show_view_set.py
+++ planetarium/tests/test_astronomy_show_view_set.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+
+from PIL import Image
 from django.test import TestCase
 
 from django.contrib.auth import get_user_model
@@ -5,21 +9,25 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from planetarium.models import PlanetariumDome, AstronomyShow, ShowTheme
+from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession
 from planetarium.serializers import (
-    PlanetariumDomeSerializer,
-    PlanetariumDomeListSerializer,
     AstronomyShowListSerializer,
     AstronomyShowDetailSerializer,
 )
 
 ASTRONOMY_SHOW_URL = reverse("planetarium:astronomyshow-list")
+SHOW_SESSION_URL = reverse("planetarium:showsession-list")
 
 
-def detail_url(astronomy_show_id: int):
+def detail_url(astro_show_id: int):
     return reverse(
-        "planetarium:astronomyshow-detail", args=[astronomy_show_id]
+        "planetarium:astronomyshow-detail", args=[astro_show_id]
     )
+
+
+def image_upload_url(astro_show_id):
+    """Return URL for recipe image upload"""
+    return reverse("planetarium:astronomyshow-upload-image", args=[astro_show_id])
 
 
 def sample_astronomy_show(**params):
@@ -30,6 +38,25 @@ def sample_astronomy_show(**params):
     }
     defaults.update(params)
     return AstronomyShow.objects.create(**defaults)
+
+
+def sample_show_session(**params):
+    planetarium_dome = PlanetariumDome.objects.create(
+        name="planetarium",
+        address="any address",
+        city_state_province="any province",
+        country="any country",
+        rows=20,
+        seats_in_row=20
+    )
+    defaults = {
+        "show_time": "2022-06-02 14:00:00",
+        "astronomy_show": sample_astronomy_show(),
+        "planetarium_dome": planetarium_dome,
+    }
+    defaults.update(params)
+
+    return ShowSession.objects.create(**defaults)
 
 
 def sample_theme(**params):
@@ -159,3 +186,94 @@ class AdminAstronomyShowApiTests(TestCase):
         self.assertEqual(theme.count(), 2)
         self.assertIn(theme_1, theme)
         self.assertIn(theme_2, theme)
+
+
+class MovieImageUploadTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_superuser(
+            "astronaut@myproject.com", "password"
+        )
+        self.client.force_authenticate(self.user)
+        self.astronomy_show = sample_astronomy_show()
+        self.theme = sample_theme()
+        self.show_session = sample_show_session(astronomy_show=self.astronomy_show)
+
+    def tearDown(self):
+        self.astronomy_show.image.delete()
+
+    def test_upload_image_to_astro_show(self):
+        """Test uploading an image to movie"""
+        url = image_upload_url(self.astronomy_show.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(url, {"image": ntf}, format="multipart")
+        self.astronomy_show.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("image", res.data)
+        self.assertTrue(os.path.exists(self.astronomy_show.image.path))
+
+    def test_upload_image_bad_request(self):
+        """Test uploading an invalid image"""
+        url = image_upload_url(self.astronomy_show.id)
+        res = self.client.post(url, {"image": "not image"}, format="multipart")
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_image_to_astro_show_list_should_not_work(self):
+        url = ASTRONOMY_SHOW_URL
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {
+                    "title": "Title",
+                    "description": "Description",
+                    "duration": 90,
+                    "theme": [1],
+                    "image": ntf,
+                },
+                format="multipart",
+            )
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        astro_show = AstronomyShow.objects.get(title="Title")
+        self.assertFalse(astro_show.image)
+
+    def test_image_url_is_shown_on_astro_show_detail(self):
+        url = image_upload_url(self.astronomy_show.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(detail_url(self.astronomy_show.id))
+
+        self.assertIn("image", res.data)
+
+    def test_image_url_is_shown_on_astro_show_list(self):
+        url = image_upload_url(self.astronomy_show.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(ASTRONOMY_SHOW_URL)
+
+        self.assertIn("image", res.data[0].keys())
+
+    def test_image_url_is_shown_on_show_session_detail(self):
+        url = image_upload_url(self.astronomy_show.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(SHOW_SESSION_URL)
+
+        self.assertIn("astro_show_image", res.data[0].keys())
