232f7b868a07d1e22dca2802c8ce371c8ecc6ed3
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-09-05 19:42:28 +0300

    fix README file, code cleanup.

--- planetarium_services/settings.py
+++ planetarium_services/settings.py
@@ -95,13 +95,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-"""Connect to sqlite3, for develop only"""
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
