7793f892ca4bea42e5cb5dabfaf0e926189a69c2
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-09-06 21:21:02 +0300

    add to settings.py python-dotenv

--- planetarium_services/settings.py
+++ planetarium_services/settings.py
@@ -13,6 +13,10 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-*le^l1@e+6zi35bx!z-7t+lvy3-lteo6h69%u9j3b!j@j)gmv^"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-*le^l1@e+6zi35bx!z-7t+lvy3-lteo6h69%u9j3b!j@j)gmv^",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
