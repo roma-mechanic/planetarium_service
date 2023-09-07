5dc77589257a36e18b774959619a37b1b2fd728d
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-09-07 16:41:32 +0300

    delete secret key from settings.py

--- planetarium_services/settings.py
+++ planetarium_services/settings.py
@@ -24,10 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-*le^l1@e+6zi35bx!z-7t+lvy3-lteo6h69%u9j3b!j@j)gmv^",
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
